94ba8ac0376c4e50dc091a9f82ed9ac718ffda12
Xxiled <93330009+xxiled-plastic-cat@users.noreply.github.com>
2025-07-04 10:20:43 +0100

    removed storybook

--- index.js
+++ index.js
@@ -3,15 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
+import App from './App';
 import { name as appName } from './app.json';
 
-// Toggle between Storybook and main app
-const enableStorybook = process.env.STORYBOOK_ENABLED === 'true';
-
-if (enableStorybook) {
-  const StorybookApp = require('./App.storybook').default;
-  AppRegistry.registerComponent(appName, () => StorybookApp);
-} else {
-  const App = require('./App').default;
-  AppRegistry.registerComponent(appName, () => App);
-}
+AppRegistry.registerComponent(appName, () => App);
